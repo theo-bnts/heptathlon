7d0606749d2a40c0a0e1dead8c15c425c8ca077c
DorianDescamps <93372714+DorianDescamps@users.noreply.github.com>
2024-06-27 10:22:36 +0200

    fixe tables

---
+++ client-front/src/main/java/fr/bnts/heptathlon/client_front/adminScreen.java
@@ -0,0 +1,11 @@
+package fr.bnts.heptathlon.client_front;
+
+import javax.swing.*;
+
+public class adminScreen {
+    private JPanel panel1;
+    private JButton consulterLesStocksButton;
+    private JButton calculerChiffreDAffairesButton;
+    private JButton listerArticlesButton;
+    private JButton listerFacturesButton;
+}
---
+++ client-front/src/main/java/fr/bnts/heptathlon/client_front/homeScreen.java
@@ -0,0 +1,9 @@
+package fr.bnts.heptathlon.client_front;
+
+import javax.swing.*;
+
+public class homeScreen {
+    private JButton écranCaisseButton;
+    private JPanel panel1;
+    private JButton écranAdministrateurButton;
+}
