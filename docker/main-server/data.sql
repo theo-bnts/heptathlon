06797c699997594ce58db669ff6daec82cbefc6f
Théo Bontemps <theo2bont@gmail.com>
2024-06-27 13:36:37 +0200

    wip

--- docker/main-server/data.sql
+++ docker/main-server/data.sql
@@ -3,9 +3,9 @@ INSERT INTO PRODUCT_CATEGORY (ID_PRODUCT_CATEGORY, NAME) VALUES
 	 (2, 'Natation');
 
 INSERT INTO PRODUCT (ID_PRODUCT, NAME, PRICE, QUANTITY, ID_PRODUCT_CATEGORY) VALUES
-	 ('ASQZDD', 'Maillot de bain', 20.0, 60, 2), 
-	 ('IGEFPI', 'Selle de cheval', 260.0, 5, 1), 
-	 ('NKKMIP', 'Lunettes de piscine', 9.0, 30, 2), 
-	 ('OIHQZD', 'Cravache', 25.0, 32, 1), 
-	 ('PHZDZE', 'Casque', 40.0, 30, 1), 
-	 ('ZODIHZ', 'Bonnet de bain', 9.0, 40, 2);
+	 ('ASQZDD', 'Maillot de bain', 20.0, -1, 2), 
+	 ('IGEFPI', 'Selle de cheval', 260.0, -1, 1), 
+	 ('NKKMIP', 'Lunettes de piscine', 9.0, -1, 2), 
+	 ('OIHQZD', 'Cravache', 25.0, -1, 1), 
+	 ('PHZDZE', 'Casque', 40.0, -1, 1), 
+	 ('ZODIHZ', 'Bonnet de bain', 9.0, -1, 2);
